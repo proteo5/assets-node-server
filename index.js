0a04720499ac28aa427c5024a071ada354a1c80e
Alfredo Pinto Molina <proteo5@hotmail.com>
2017-06-05 23:59:32 -0700

    Layouts for pages added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const mustache = require('mustache');
 // you can pass the parameter in the command line. e.g. node static_server.js 3000
 const port = process.argv[2] || 9000;
 http.createServer(function(req, res) {
@@ -39,6 +40,9 @@ http.createServer(function(req, res) {
     //If exist on the public folder
     if (fileInPublic.exist) {
         sendPublicFile(fileInPublic.pathname, res);
+    } else if (fileInPages.exist) {
+        //If exist on the page folder
+        sendPage(fileInPages.pathname, res);
     } else {
         // if the file is not found, return 404
         res.statusCode = 404;
@@ -92,5 +96,48 @@ var sendPublicFile = function(pathname, res) {
     } catch (error) {
         res.statusCode = 500;
         res.end(`Error getting the file: ${error}.`);
+    }
+};
+
+//Function to send a file on the public folder
+var sendPage = function(pathname, res) {
+    try {
+        // read file from file system
+        var data = fs.readFileSync(pathname, "utf8");
+        // split files by lines
+        var dataLines = data.split(/\r?\n/);
+
+        //check first line if it has a layout
+        var hasLayout = dataLines[0].indexOf("<!-- layout:") !== -1;
+        if (hasLayout) {
+            var layout = getLayout(dataLines[0]);
+            var body = "";
+            for (var index = 1; index < dataLines.length; index++) {
+                body += dataLines[index] + "\n";
+            }
+            data = mustache.to_html(layout, { body: body });
+        }
+
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', 'text/html');
+        res.end(data);
+    } catch (error) {
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${error}.`);
+    }
+};
+
+var getLayout = function(layoutInfo) {
+    try {
+        var data = '{{{body}}}'
+        var pathname = layoutInfo.replace('<!-- layout: ', '').replace(' -->', '').replace(' ', '');
+
+        var fileInfo = verifyPath(`./layouts/${pathname}`);
+        if (fileInfo.exist) {
+            data = fs.readFileSync(fileInfo.pathname, "utf8");
+        };
+        return data;
+    } catch (error) {
+        return "{{{body}}}";
     }
 };
